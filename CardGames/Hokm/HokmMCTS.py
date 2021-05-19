26f8775d8fc74dc6763720f619e0db7410df5d48
Vahid <vd1371@gmail.com>
2021-05-19 17:53:26 +0800

    dividing HokmMCTS

--- CardGames/Hokm/HokmMCTS.py
+++ CardGames/Hokm/HokmMCTS.py
@@ -56,21 +56,19 @@ class HokmTableForMCTS:
 			next_player = (next_player + 1) % self.settings.n_players
 
 	def mcts_play_one_round(self, table=None, action=None, played_cards = None, n_round = 1):
+
 		# add the on_table cards on the table
 		if table is None:
 			table = []
+			# key: player, value: card
+			played_cards = {}
 
-		played_cards = {}  # key: player, value: card
-		i = self.hakem
+		else:
+			played_cards = deepcopy(played_cards)
 
-		if len(table) > 0:
-			for card in table:
-				played_cards[card] = (i, i % self.settings.n_players)
-				i = i + 1
-
-		n_iter = self.settings.n_players if len(table) > 0 else self.settings.n_players - len(table)
+		# n_iter = self.settings.n_players if len(table) > 0 else self.settings.n_players - len(table)
 		
-		for i in range(n_iter):
+		for i in range(self.settings.n_players - len(played_cards)):
 			turn = (self.turn + i) % self.settings.n_players
 
 			if self.turn == turn and not action is None:
@@ -82,8 +80,10 @@ class HokmTableForMCTS:
 			# updating the table
 			table.append(action)
 
-			# key: card, value( i = the i th played card, turn = by global player number)
+			# key: card, value( i = the i th played card, turn = by global player number).
 			played_cards[action] = (i, turn)
+			# print (played_cards)
+			# input()
 
 		round_winner, rewards = self._analyze_round(played_cards)
 		# updating the knowledge of player of played cards
@@ -110,16 +110,17 @@ class HokmTableForMCTS:
 
 
 
-def HokmMCTS(memory, hand, table, played_cards, possible_cards):
+def HokmMCTS(memory, hand, table, played_cards, possible_cards, logger):
 	# n_mcts_sims -> the number of play to decide but select card randomly
 	''' Monte Carlo Tree Search for Hokm'''
 
 	start = time.time()
-	probabilities = np.zeros(len(possible_cards))
+	wins = np.zeros(len(possible_cards))
+	used = np.zeros(len(possible_cards))
 
 	# This is derived for finding the number of mcts
 	n_round = int(13 - memory['my_score'] - memory['other_score'])
-	n_mcts_sims = (n_round - 1) * 10
+	n_mcts_sims = (n_round - 1) * 100
 
 	# If there is only one option, just return that card
 	if len(possible_cards) == 1:
@@ -144,18 +145,20 @@ def HokmMCTS(memory, hand, table, played_cards, possible_cards):
 
 			n_round = int(memory['my_score'] + memory['other_score'])
 			# instantiate the players with the deck and make them all play randomly (strategy = "random")
-			p0 = HokmPlayer(name='AlexRandom', deck=deck, settings=HokmSettings, strategy='random')
-			p1 = HokmPlayer(name='RyanRandom', deck=deck, settings=HokmSettings, strategy='random')
-			p2 = HokmPlayer(name='JimmyRandom', deck=deck, settings=HokmSettings, strategy='random')
-			p3 = HokmPlayer(name='MathewRandom', deck=deck, settings=HokmSettings, strategy='random')
+			p0 = HokmPlayer(name='AlexRandom', deck=deck, settings=HokmSettings, strategy='random', logger = logger)
+			p1 = HokmPlayer(name='RyanRandom', deck=deck, settings=HokmSettings, strategy='random', logger = logger)
+			p2 = HokmPlayer(name='JimmyRandom', deck=deck, settings=HokmSettings, strategy='random', logger = logger)
+			p3 = HokmPlayer(name='MathewRandom', deck=deck, settings=HokmSettings, strategy='random', logger = logger)
 
 			# randomly select a card from possible_cards
 			selected_card = np.random.choice(possible_cards)
 			idx = possible_cards.index(selected_card)
+			used[idx] += 1
 
+			temp_hand = deepcopy(hand)
 			temp_table = deepcopy(table)
 			temp_deck = deepcopy(deck)
-			temp_hand = deepcopy(hand)
+			temp_deck.shuffle()
 
 			# instantiate a new hokm table
 			hokm_table = HokmTableForMCTS(p0, p1, p2, p3,
@@ -167,27 +170,21 @@ def HokmMCTS(memory, hand, table, played_cards, possible_cards):
 			hokm_table.mcts_initialize(temp_hand)
 
 			# play rounds
-			temp_round_winner, temp_rewards = hokm_table.mcts_play_one_round(temp_table,
-																			 selected_card,
-																			 played_cards,
-																			 n_round)
+			temp_round_winner, temp_rewards = hokm_table.mcts_play_one_round(table = temp_table,
+																			 action = selected_card,
+																			 played_cards = played_cards,
+																			 n_round = n_round)
 			while not hokm_table.game_over():
 				n_round += 1
 				temp_round_winner = hokm_table.mcts_play_one_round(n_round = n_round)
 
-			p0_sum = hokm_table.players[0].my_score
-			p1_sum = hokm_table.players[1].my_score
 
-			tt = org_turn
-			if (org_turn > 1):
-				tt = (org_turn + 2) % 4
-
-
-			if (p0_sum > p1_sum) and (tt == 0):
-				probabilities[idx] += 1
-			if (p0_sum < p1_sum) and (tt == 1):
-				probabilities[idx] += 1
+			if hokm_table.players[org_turn].my_score > \
+					hokm_table.players[org_turn].other_score:
+				wins[idx] += 1
 
 		# find the card with highest probability of winning and return it
-		print (f"MCTS : {time.time()-start:.2f} <<<<<<<<<<<<<<<<<<<")
-		return possible_cards[np.argmax(probabilities)]
+		logger.info(f"nums {used}")
+		logger.info(f"probs {wins/used}")
+
+		return possible_cards[np.argmax(wins/used)]
